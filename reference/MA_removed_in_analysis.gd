d87c109f887beff0e0facefdcb3f87a25a038df2
jeffreybarrick <jeffrey.e.barrick@gmail.com>
2020-02-22 15:23:45 -0600

    Added reference files

---
+++ reference/MA_removed_in_analysis.gd
@@ -0,0 +1,6 @@
+#=GENOME_DIFF	1.0
+#=TITLE	MA_removed_in_analysis
+#=AUTHOR	Barrick JE
+#=REFSEQ	BarrickLab-Private:genomes/reference/ADP1-WT.gff3
+#=COMMENT	Hypermutable simple sequence repeat mutation shared by most MA lines that may have been present or polymorphic in the MA ancestor
+INS	1	.	ADP1-WT	178467	G	repeat_length=1	repeat_new_copies=10	repeat_ref_copies=9	repeat_seq=G
